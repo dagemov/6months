1e1188a67f01ff7c48cacc8852216fb91452770f
dagemov <97604053+dagemov@users.noreply.github.com>
2026-05-01 10:07:23 -0400

    feat : chore added basic CompanyMemberShipStatus

---
+++ DagemovApp/src/Dagemov.Domain/Enums/CompanyMemberShipStatus.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dagemov.Domain.Enums;
+
+public enum CompanyMemberShipStatus
+{
+    Activated,
+    Disactivated,
+    Expired,
+    Cancel,
+    Prosessing
+}
