44cad1a867f51306224c91d899a3c087d3565e2d
dagemov <97604053+dagemov@users.noreply.github.com>
2026-05-03 07:27:26 -0400

    chore : entities configurations in fluent Api

---
+++ DagemovApp/src/Dagemov.Persistence/Configurations/WorkShiftConfiguration.cs
@@ -0,0 +1,46 @@
+﻿using Dagemov.Domain.Entities;
+using Dagemov.Domain.ValueObjects;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Dagemov.Persistence.Configurations;
+
+public class WorkShiftConfiguration : IEntityTypeConfiguration<WorkShift>
+{
+    public void Configure(EntityTypeBuilder<WorkShift> builder)
+    {
+        builder.HasKey(id => id.Id);
+
+        builder.Property(description => description.Description)
+            .IsRequired(false)
+            .HasMaxLength(180);
+
+        builder.Property(breakTime => breakTime.BreakTime).
+            IsRequired(true);
+    
+      
+        builder.OwnsOne(x => x.ShiftPeriod, period =>
+        {
+            period.Property(p => p.Startshift)
+                .HasColumnName("StartShift")
+                .IsRequired();
+
+            period.Property(p => p.EndShift)
+                .HasColumnName("EndShift")
+                .IsRequired();
+        });
+
+
+        builder.Property(x => x.CreatedDate)
+            .IsRequired();
+
+        builder.Property(x => x.UpdatedDate)
+            .IsRequired();
+
+        builder.Property(x => x.ModifiedByUser)
+            .IsRequired(false)
+            .HasMaxLength(150);
+
+    }
+    
+}
