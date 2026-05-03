a61869b5475e059b35edc548cc7bc214b83f1d79
dagemov <97604053+dagemov@users.noreply.github.com>
2026-05-03 07:30:05 -0400

    chore :  Abstrac audit patron added

---
+++ DagemovApp/src/Dagemov.Domain/Interfaces/IAuditableEntity.cs
@@ -0,0 +1,11 @@
+﻿namespace Dagemov.Domain.Interfaces;
+
+public interface IAuditableEntity
+{
+    DateTime CreatedDate { get; }
+    DateTime UpdatedDate { get; }
+    string? ModifiedByUser { get; }
+
+    void MarkCreated(DateTime date, string userName);
+    void MarkUpdated(DateTime date, string userName);
+}
